30bbd147ac6e5e47bc03f4e7f046909c22672956
fierylion <mawalladaniel2018@gmail.com>
2023-04-11 13:29:57 +0300

    added requirements

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from TweetRetweetDeleter.delete_tweets import DeleteTweets
 # after importing use it as
 
-DeleteTweets('daniel_mawalla', '*{1601}*#', 'hello world this is danielkapfeijf')
+#DeleteTweets('my_username', 'my_password', 'my_message')
